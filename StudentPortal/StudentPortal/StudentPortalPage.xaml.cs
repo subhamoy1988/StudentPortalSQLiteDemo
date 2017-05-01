3d555c7dad23e3661b300077a28eda04347777e8
Subhamoy Burman <sburman@ciber.com>
2017-05-01 16:26:07 -0700

    SQLite included initial checkin

---
+++ StudentPortal/StudentPortal/StudentPortalPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StudentPortal
+{
+	public partial class StudentPortalPage : ContentPage
+	{
+		public StudentPortalPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
